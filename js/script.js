03172fe5c571160ea71ba943acc0da2b63d2c069
lorenap15a <lorenap.lp26@gmail.com>
2022-05-28 16:12:26 -0500

    Actividad 7

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function cualeselpais10(params) {
+
+    var cualeselpais10 = prompt("¿Cuál es el país número 10 más globalizado?")
+
+if (cualeselpais10 == "Francia") {
+    alert("¡Muy bien!")
+} else {
+    alert("Ups, sigue intentando")
+}
+    
+}
